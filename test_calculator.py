621721d17c8200b96e0c9b8eb9aee34810589190
Nidhi <cnidhishree8@gmail.com>
2025-04-22 10:53:51 +0530

    Made few changes

--- test_calculator.py
+++ test_calculator.py
@@ -5,6 +5,7 @@ class TestCalculator(unittest.TestCase):
 
     def test_add(self):
         self.assertEqual(add(2, 3), 5)
+        print("Hi")
 
     def test_subtract(self):
         self.assertEqual(subtract(5, 3), 2)
